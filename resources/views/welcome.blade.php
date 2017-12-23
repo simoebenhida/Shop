fbd14b51250cfab554fd466441402fe8070047fd
mohamed <mohameedbenhida@gmail.com>
2017-12-23 22:48:14 +0000

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Welcome</title>
+</head>
+
+<script src="{{ mix('js/app.js') }}"></script>
+</html>
